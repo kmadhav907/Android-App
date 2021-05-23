2ec63764b87df5d29319b4b11eb12a2c8bc6d382
kmadhav907 <kmadhav907@gmail.com>
2021-05-23 12:03:15 +0530

    Online/Offline Realtime Changes

--- app/src/main/java/com/example/messenger/Adapters/UsersAdapter.java
+++ app/src/main/java/com/example/messenger/Adapters/UsersAdapter.java
@@ -84,6 +84,7 @@ public class UsersAdapter extends  RecyclerView.Adapter<UsersAdapter.UserViewHol
            public void onClick(View v) {
                Intent intent = new Intent(context, ChatActivity.class);
                intent.putExtra("name", user.getName());
+               intent.putExtra("image", user.getProfileImage());
                intent.putExtra("uid", user.getUid());
                context.startActivity(intent);
            }
